08f88c909392747e7aa95025d5dac7eea0de2de6
Yvan Manzi <113548523+yvanmanz1@users.noreply.github.com>
2023-10-08 13:42:50 +0200

    Update Doctor.cs

--- Doctor.cs
+++ Doctor.cs
@@ -35,7 +35,7 @@ namespace PatientManagementSys
 
             dataGridView.DataSource = dataTable;
         }
-        SqlConnection conn = new SqlConnection(@"Data Source=Yvan-PC\SQLEXPRESS;Initial Catalog=StudentPortalDB;Integrated Security=True");
+        SqlConnection conn = new SqlConnection(@"Data Source=Yvan-PC\SQLEXPRESS;Initial Catalog=HospitalPortalDB;Integrated Security=True");
         private void button1_Click(object sender, EventArgs e)
         {
             Handle dh = new Handle();
